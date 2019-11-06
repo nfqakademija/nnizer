7a5593bcd6870080283e68a69f9c413e6d39dfd2
Dominykas <dominykasmurauskas1@gmail.com>
2019-11-06 13:38:09 +0200

    Phone number validation re-configured and unnecessary namespaces deleted

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,16 +5,14 @@ namespace App\Form;
 use App\Entity\Contractor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\IsTrue;
 
 class RegistrationFormType extends AbstractType
 {
@@ -89,7 +87,7 @@ class RegistrationFormType extends AbstractType
                     ])
                 ]
             ])
-            ->add('phoneNumber', NumberType::class, [
+            ->add('phoneNumber', TelType::class, [
                 'label' => 'registration_form.phoneNumber',
             ])
         ;
