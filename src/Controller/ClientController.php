92c5bd6758b278aabb4c7b935214d2ec0e94c667
Dominykas <dominykasmurauskas1@gmail.com>
2019-11-08 10:36:54 +0200

    Swiftmailer installed

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Form\ClientRegistrationFormType;
+use Exception;
+use Swift_Mailer;
+use Swift_SmtpTransport;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,11 +18,13 @@ class ClientController extends AbstractController
     /**
      * @Route("/new-client", name="app_client_register")
      * @param Request $request
+     * @param Swift_Mailer $mailer
      * @return Response
-     * @throws \Exception
+     * @throws Exception
      */
     public function register(
-        Request $request
+        Request $request,
+        Swift_Mailer $mailer
     ): Response {
         $user = new Client();
         $form = $this->createForm(ClientRegistrationFormType::class, $user);
@@ -33,6 +38,7 @@ class ClientController extends AbstractController
             $entityManager->flush();
 
             // do anything else you need here, like send an email
+            $this->sendActivationEmail($user, $mailer);
 
             return new RedirectResponse($this->generateUrl('home'));
         }
@@ -40,5 +46,27 @@ class ClientController extends AbstractController
         return $this->render('client/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @param Client $client
+     * @param Swift_Mailer $mailer
+     */
+    public function sendActivationEmail(Client $client, Swift_Mailer $mailer): void
+    {
+        $message = (new \Swift_Message('NNIZER reservation'))
+            ->setFrom('nnizernfq@gmail.com')
+            ->setTo($client->getEmail())
+            ->setBody(
+                $this->renderView(
+                    'emails/client-register.html.twig',
+                    ['name' => $client->getFirstname()]
+                ),
+                'text/html'
+            )
+        ;
+
+        $mailer->send($message);
+
     }
 }
