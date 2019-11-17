fc3e987879289f1ac60c07a1353ea8e9766fda4f
Kornelijus Glinskas <Kornelijus@veviam.lt>
2019-11-17 19:27:23 +0200

    connected react

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app', [
+        './assets/js/app.js',
+        './assets/js/react.jsx'
+    ])
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -57,6 +60,7 @@ Encore
 
     // enables Sass/SCSS support
     .enableSassLoader()
+    .enableReactPreset()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
