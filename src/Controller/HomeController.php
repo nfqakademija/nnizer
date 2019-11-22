953b6162c6a7a915c311f52f9ae2380b3e4cd36a
Dominykas <dominykasmurauskas1@gmail.com>
2019-11-22 20:37:17 +0200

    Visit can now choose website language

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija [pulled]',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/lang/{lang}", name="language")
+     * @param Request $request
+     * @param $lang
+     * @return Response
+     */
+    public function setLanguage(Request $request, $lang): Response
+    {
+        if ($request->headers->get('referer') && $lang !== null) {
+            $request->getSession()->set('_locale', $lang);
+            
+            return $this->redirect($request->headers->get('referer'));
+        } else {
+            return $this->redirectToRoute('home');
+        }
     }
 }
