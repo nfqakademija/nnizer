78998fc644c91fd3136e2b76ebaaa646bbb759a1
Dominykas <dominykasmurauskas1@gmail.com>
2019-11-23 16:39:16 +0200

    Manual logout implementation

--- src/Controller/ContractorAuthController.php
+++ src/Controller/ContractorAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -29,9 +30,16 @@ class ContractorAuthController extends AbstractController
 
     /**
      * @Route("/logout", name="app_logout")
+     * @param Request $request
+     * @return Response
      */
-    public function logout()
+    public function logout(Request $request): Response
     {
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+        $lang = $request->getSession()->get('_locale');
+        $this->get('security.token_storage')->setToken(null);
+        $request->getSession()->invalidate();
+        $request->getSession()->set('_locale', $lang);
+
+        return $this->redirectToRoute('home');
     }
 }
