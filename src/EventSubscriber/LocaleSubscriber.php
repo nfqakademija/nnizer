f7fb9ffbae050ce764b179b8cb1f8883dc6d29fe
Dominykas <dominykasmurauskas1@gmail.com>
2019-11-24 16:50:55 +0200

    [HOTFIX] Default language

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -23,11 +23,7 @@ class LocaleSubscriber implements EventSubscriberInterface
             return;
         }
 
-        if ($locale = $request->attributes->get('_locale')) {
-            $request->getSession()->set('_locale', $locale);
-        } else {
-            $request->setLocale($request->getSession()->get('_locale'));
-        }
+        $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
     }
 
     public static function getSubscribedEvents()
