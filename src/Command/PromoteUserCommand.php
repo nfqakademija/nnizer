d22c733214e16fa53bdcf5d506c0522d0411dc41
Migle <kr.migle@gmail.com>
2019-12-05 01:04:53 +0200

    Admin login and EasyAdminBundle

---
+++ src/Command/PromoteUserCommand.php
@@ -0,0 +1,74 @@
+<?php
+namespace App\Command;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Security\Core\User\UserInterface;
+class PromoteUserCommand extends Command
+{
+    protected static $defaultName = 'app:promote-user';
+    /** @var EntityManagerInterface */
+    private $entityManager;
+    private $adminRole;
+    /**
+     * PromoteUserCommand constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager, $adminRole = 'ROLE_ADMIN')
+    {
+        $this->entityManager = $entityManager;
+        $this->adminRole = $adminRole;
+        parent::__construct();
+    }
+    protected function configure()
+    {
+        $this
+            ->setDescription('Make user admin')
+            ->addArgument('email', InputArgument::REQUIRED, 'E-mail address of existing user');
+    }
+    /**
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Parsing input
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        // Getting user
+        $this->info("Searching for user", $email, $io);
+        $user = $this->entityManager->getRepository(User::class)->findByEmail($email);
+        if (!$user) {
+            $io->error("Cannot find user by e-mail: " . $email);
+            return;
+        }
+        if (in_array($this->adminRole, $user->getRoles())) {
+            $this->printUserRoles($user, $io);
+            $io->success('Admin role already exists');
+            return;
+        }
+        // Adding admin role
+        $this->info("Adding role: ", $this->adminRole, $io);
+        $roles = $user->getRoles();
+        $roles[] = $this->adminRole;
+        $user->setRoles(array_unique($roles));
+        // Storing user
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        $this->printUserRoles($user, $output);
+        $io->success('Admin role successfully added');
+    }
+    private function info($message, $value, OutputInterface $io)
+    {
+        $io->writeln(sprintf('<info>%s</info>: <comment>%s</comment>', $message, $value));
+    }
+    private function printUserRoles(UserInterface $user, OutputInterface $io)
+    {
+        $io->writeln(
+            "<info>User roles</info>: <comment>" . join('</comment>, <comment>', $user->getRoles()) . '</comment>'
+        );
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string)$this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string)$this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Gets triggered only on insert
+     * @ORM\PrePersist
+     */
+    public function onPrePersist()
+    {
+        $this->createdAt = new \DateTime("now");
+    }
+
+    /**
+     * Gets triggered every time on update
+     * @ORM\PreUpdate
+     */
+    public function onPreUpdate()
+    {
+        $this->updatedAt = new \DateTime("now");
+    }
+}
