2a2632a44d713a33eaee13669f67973097c31bd0
Dominykas <dominykasmurauskas1@gmail.com>
2019-12-10 12:29:12 +0200

    Created fixture groups & added more realistic data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,12 +6,12 @@ use App\Entity\Contractor;
 use App\Entity\ContractorSettings;
 use App\Entity\Reservation;
 use App\Entity\Review;
-use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class AppFixtures extends Fixture
+class AppFixtures extends Fixture implements FixtureGroupInterface
 {
     /**
      * @var array
@@ -53,7 +53,6 @@ class AppFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
-        $this->loadUsers($manager, $this->encoder);
         $this->loadContractors($manager, $this->encoder);
 
         $manager->flush();
@@ -63,39 +62,31 @@ class AppFixtures extends Fixture
      * @param ObjectManager $manager
      * @param UserPasswordEncoderInterface $encoder
      */
-    private function loadUsers(ObjectManager $manager, UserPasswordEncoderInterface $encoder)
-    {
-        $names = ['nfq', 'dominykas', 'kornelijus', 'migle', 'tadas'];
-        foreach ($names as $name) {
-            $user = new User();
-            $user->setEmail($name . '@' . $name . '.com');
-            $user->setRoles(['ROLE_ADMIN']);
-            $user->setPassword($encoder->encodePassword($user, $name));
-            $user->setName($name);
-            $manager->persist($user);
-        }
-    }
-
-    /**
-     * @param ObjectManager $manager
-     * @param UserPasswordEncoderInterface $encoder
-     */
     private function loadContractors(ObjectManager $manager, UserPasswordEncoderInterface $encoder)
     {
-        $services = ['Massages', 'Hairdressing', 'Driving', 'Teaching'];
+        $services = ['Massages', 'Hairdressing', 'Driving services', 'Teaching'];
+        $firstnames = $this->getFirstnames();
+        $lastnames = $this->getLastnames();
+        $descriptions = $this->getDescriptions();
+
         for ($i = 1; $i < 51; $i++) {
             try {
+                $firstname = $firstnames[random_int(0, 9)];
+                $lastname = $lastnames[random_int(0, 9)];
+                $serviceTitle = $services[random_int(0, 3)];
+                $description = $descriptions[random_int(0, 3)];
+
                 $contractor = new Contractor();
                 $contractor->setPassword($encoder->encodePassword($contractor, 'fixture'));
-                $contractor->setFirstname('Contractor');
-                $contractor->setLastName('Fixture ' . $i);
-                $contractor->setUsername('fixture' . $i);
-                $contractor->setEmail('contractor' . $i . '@' . $i . '.com');
+                $contractor->setFirstname($firstname);
+                $contractor->setLastName($lastname);
+                $contractor->setUsername($firstname . $i);
+                $contractor->setEmail($firstname . '@' . $lastname . '.com');
                 $contractor->setPhoneNumber(random_int(860000000, 869999999));
                 $contractor->setVerificationKey();
                 $contractor->setIsVerified(random_int(0, 1));
-                $contractor->setTitle($services[random_int(0, 3)]);
-                $contractor->setDescription('There\'s not much to tell. Try it out and tell me what you think');
+                $contractor->setTitle($serviceTitle);
+                $contractor->setDescription($description);
                 $this->loadSettings($contractor, $manager);
                 $this->loadReservations($contractor, $manager);
                 $this->loadReviews($contractor, $manager);
@@ -169,13 +160,92 @@ class AppFixtures extends Fixture
             $review = new Review();
             try {
                 $review->setContractor($contractor);
-                $review->setDescription('Don\'t take it serious, i\'m a fixture!');
+                $review->setDescription('The rating tells it all!');
                 $review->setReservation($reservation);
-                $review->setStars(random_int(0, 5));
+                $review->setStars(random_int(1, 5));
                 $manager->persist($review);
                 $contractor->addReview($review);
             } catch (\Exception $e) {
             }
         }
+    }
+
+    /**
+     * @return array
+     */
+    private function getFirstnames(): array
+    {
+        return [
+            'Jerry',
+            'Jesse',
+            'Daniel',
+            'Shirley',
+            'Helen',
+            'James',
+            'Ben',
+            'Stewart',
+            'Carol',
+            'Wayne',
+            'William',
+            'Gerald',
+            'Kai',
+            'Albert',
+            'Brian',
+            'Benjamin',
+            'Kevin'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getLastnames(): array
+    {
+        return [
+            'Evans',
+            'Holmes',
+            'Hopkins',
+            'Fowler',
+            'Murphy',
+            'Ray',
+            'Thompson',
+            'Garcia',
+            'Brooks',
+            'Phillips',
+            'Palmer',
+            'Hawkins',
+            'Wilson',
+            'Castro',
+            'Weber',
+            'Little'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getDescriptions(): array
+    {
+        return [
+            'Hey there! I\'m a professional that you certainly need. With experience of over 10 years in the industry' .
+            ' I\'m proud to say that I am one of the best. Here\'s the best part - if you do not really like my ' . '
+            services, You don\'t have to pay. Sign up now and let me prove you how good I am!',
+            'I\'ve been working in the field since 2000 and since then I did not have a single disappointed customer.' .
+            'What is more, if you ever have difficulties coming to my place, I can show up at yours and provide as ' .
+            'good service as it can be. ',
+            'Since you are here, let me introduce myself. I\'m a 35 year old male currently living in USA and this ' .
+            'is what I am doing for living. With over 5000 customers satisfied, I\'m feeling confident in what I am' .
+            'doing. Sign up now and let me show you what kind of quality you deserve. ',
+            'I\'ve learned everything from my mentors in my young days and now I provide top quality services in this' .
+            'town. Feel free to check it out!',
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getGroups(): array
+    {
+        return ['contractors'];
     }
 }
