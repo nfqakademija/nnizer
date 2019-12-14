404804404b1435db6146a9a4333ff875b9efd797
Kornelijus Glinskas <Kornelijus@veviam.lt>
2019-12-14 12:54:00 +0200

    handled response on frontend

--- assets/js/contractor.jsx
+++ assets/js/contractor.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
-import MicroModal from 'micromodal';
+import axios from 'axios';
+
 import Template from './components/contractor/Template';
 
-MicroModal.init({
-  disableScroll: true,
-  disableFocus: false,
-  awaitOpenAnimation: true,
-  awaitCloseAnimation: true,
+render(<Template />, document.getElementById('contractor'));
+
+const baseURL = `${window.location.protocol}//${window.location.host}`;
+
+const bookForm = document.querySelector('.js-book-form');
+// const formData = new FormData();
+const bookBtn = document.querySelector('.js-book-btn');
+
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // How to send data properly? select value?
+  const date = document.querySelector('#datepicker input').value;
+  const firstName = document.querySelector('.js-book-firstname').value;
+  const secondName = document.querySelector('.js-book-lastname').value;
+  const email = document.querySelector('.js-book-email').value;
+
+  const formData = new FormData(bookForm);
+
+  axios({
+    method: 'post',
+    url: `${baseURL}/new-reservation`,
+    data: formData,
+  })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
-
-
-render(
-  <Template />,
-  document.getElementById('contractor'),
-);
