b9067ddadfc30418a0fd0894d68d03997b211db9
Kornelijus Glinskas <Kornelijus@veviam.lt>
2019-12-17 22:30:40 +0200

    added backend notification display

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Alert, showAlert } from './Utils/NotificationUtils';
+
 require('../css/app.scss');
+
+const renderAlertContainer = () => {
+  const alertContainer = document.querySelector('#backend-alert-container');
+  render(
+    <Alert />,
+    alertContainer,
+  );
+};
+
+const displayBackendAlert = () => {
+  const backendAlert = document.querySelector('.js-backend-alert');
+  if (document.body.contains(backendAlert)) {
+    const msg = backendAlert.innerHTML;
+    showAlert(msg);
+  }
+};
+
+renderAlertContainer();
+displayBackendAlert();
