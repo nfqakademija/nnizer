72097059c30a86ed0cb02a58c04e50d7f726eb3b
Dominykas <dominykasmurauskas1@gmail.com>
2019-12-18 11:54:34 +0200

    Exception listener started sending emails

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -3,9 +3,13 @@
 namespace App\EventListener;
 
 use App\Service\MailerService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ExceptionListener
 {
@@ -21,18 +25,28 @@ class ExceptionListener
     private $environment;
 
     /**
-     * ExceptionListener constructor.
-     * @param MailerService $mailer
-     * @param String $environment
+     * @var
      */
-    public function __construct(String $environment, MailerService $mailer)
+    private $twig;
+
+    /**
+     * ExceptionListener constructor.
+     * @param String $environment
+     * @param MailerService $mailer
+     * @param ContainerInterface $container
+     */
+    public function __construct(String $environment, MailerService $mailer, ContainerInterface $container)
     {
         $this->mailer = $mailer;
         $this->environment = $environment;
+        $this->twig = $container->get('twig');
     }
 
     /**
      * @param ExceptionEvent $event
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
      */
     public function onKernelException(ExceptionEvent $event)
     {
@@ -41,11 +55,18 @@ class ExceptionListener
         }
 
         $exception = $event->getException();
+        $response = new Response();
         if ($exception instanceof NotFoundHttpException) {
-            $event->setException(new NotFoundHttpException('Page not found'));
+            $response->setStatusCode($exception->getStatusCode());
+            $twig = $this->twig->render('bundles/TwigBundle/Exception/error404.html.twig');
+            $response->setContent($twig);
         } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
             $this->mailer->sendExceptionEmail($exception);
-            $event->setException(new \Exception('Unexpected error', Response::HTTP_INTERNAL_SERVER_ERROR));
+            $twig = $this->twig->render('bundles/TwigBundle/Exception/error.html.twig');
+            $response->setContent($twig);
         }
+
+        $event->setResponse($response);
     }
 }
